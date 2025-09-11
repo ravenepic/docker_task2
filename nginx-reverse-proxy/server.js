6352dea44e5552c08837e82566ba5749ef8df2e4
ravenepic <krshyamsankar@gmail.com>
2025-09-11 17:16:29 +0530

    made changes

--- nginx-reverse-proxy/server.js
+++ nginx-reverse-proxy/server.js
@@ -3,10 +3,10 @@ const app = express();
 const port = process.env.PORT;
 const server = process.env.SERVER;
 
-app.get("${server}", (req, res) => {
+app.get(`/${server}`, (req, res) => {
   res.send(`Hello from ${server} Node.js`);
 });
 
-app.listen(`${port}`, () => {
+app.listen(port, () => {
   console.log(`${server} running on port ${port}`);
 });
